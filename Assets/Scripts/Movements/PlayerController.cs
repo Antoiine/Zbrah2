c40b99c162fdf5be2970c63949a00c9fe4acf817
Antoiine <neosia@live.fr>
2018-05-22 21:08:14 -0400

    Ebauche Deplacement + Camera

---
+++ Assets/Scripts/Movements/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Scripts.Movements;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float speed = 5;
+    public float mouseSensitivity = 3;
+
+    private PlayerMovement scriptMove;
+
+	// Use this for initialization
+	void Start () {
+        scriptMove = GetComponent<PlayerMovement>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //Movement "ZQSD"
+        float _xMov = Input.GetAxis("Horizontal");
+        float _zMov = Input.GetAxis("Vertical");
+
+        Vector3 hDirection = transform.right * _xMov;
+        Vector3 zDirection = transform.forward * _zMov;
+
+        Vector3 _velocity = (hDirection + zDirection) * speed;
+        scriptMove.SetMove(_velocity);
+
+        //Horizontal Rotation
+        float _yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 _hRotation = new Vector3(0, _yRot, 0) * mouseSensitivity;
+        scriptMove.SetHorizontalRotation(_hRotation);
+
+        //Camera Rotation
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+        scriptMove.SetCameraRotation(_xRot * mouseSensitivity);
+
+
+    }
+}
---
+++ Assets/Scripts/Movements/PlayerMovement.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Movements
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        //Properties
+        private Vector3 nextMove = Vector3.zero;
+        private Vector3 hRotation = Vector3.zero;
+
+        private float camRotation = 0f;
+        private float currentCamRotation = 0f;
+        private float camRotationLimit = 85f;
+
+        private Rigidbody rb;
+        private Camera cam;
+
+        
+
+        //Call when the scene is loaded
+        public void Start()
+        {
+            rb = this.GetComponent<Rigidbody>();
+            cam = this.GetComponentInChildren<Camera>();
+        }
+
+        //Set the movement for the next frame
+        public void SetMove(Vector3 _movement)
+        {
+            nextMove = _movement;            
+        }
+
+        //Set the horizontal rotation for the next frame
+        public void SetHorizontalRotation(Vector3 _rotation)
+        {
+            hRotation = _rotation;
+        }
+
+        //Set the camera rotation for the next frame
+        public void SetCameraRotation(float _rotation)
+        {
+            camRotation = _rotation;
+        }
+
+
+        //Call once every frame
+        public void FixedUpdate()
+        {
+            //Movement
+            if(nextMove != Vector3.zero)
+            {
+                rb.MovePosition(rb.position + nextMove * Time.deltaTime);
+            }
+
+            //Horizontal Rotation
+            rb.MoveRotation(rb.rotation * Quaternion.Euler(hRotation));
+
+            //Camera Rotation
+            if(cam != null)
+            {
+                // Set our rotation and clamp it
+                currentCamRotation -= camRotation;
+                currentCamRotation = Mathf.Clamp(currentCamRotation, -camRotationLimit, camRotationLimit);
+
+                //Apply our rotation to the transform of our camera
+                cam.transform.localEulerAngles = new Vector3(currentCamRotation, 0f, 0f);
+            }            
+            
+        }
+
+    }
+}
